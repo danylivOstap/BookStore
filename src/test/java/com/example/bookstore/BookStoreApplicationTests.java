9751d1b044f3273178d2f268c5959b297ffc654c
Ostap Danyliv <danyliv.ostap@gmail.com>
2024-05-11 09:30:27 -0500

    merge with main

--- src/test/java/com/example/bookstore/BookStoreApplicationTests.java
+++ src/test/java/com/example/bookstore/BookStoreApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.bookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
 class BookStoreApplicationTests {
